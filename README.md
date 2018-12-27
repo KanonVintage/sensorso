f7aff3678b1c0bdf91420058edc4cf8f84eb9c6f
KanonVintage <guido.duchi@gmail.com>
2018-12-26 23:36:17 -0500

    concurrent updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Running
+## Compilacion
 
 Seguir las siguientes instrucciones para compilar el `sensorSO.c` y `lector.c`:
 
@@ -11,3 +11,11 @@ lector.c:
 ```shell
 gcc -g lector.c -o compiled/lector -lc
 ```
+
+concurrent.c:
+```shell
+gcc -g concurrent.c -o compiled/concurrent -lpthread
+```
+
+El trabajo consiste en subir a youtube un video de máximo 10 mins en ingles y minimo 7 minutos, que explique el problema (max 1/5 del tiempo de duracion del video), propuesta de diseño y resultados del proyecto finalizado del primer parcial. Debe de tener ayudas visuales para explicar su diseño y su justificación y mostrar resultados cuantitativos usando gráficos o tablas
+
--- concurrent.c
+++ concurrent.c
@@ -1,4 +1,4 @@
-# include <sys/types.h>
+/*# include <sys/types.h>
 # include <unistd.h>
 # include <stdio.h>
 
@@ -15,4 +15,42 @@ int main (int argc, char* argv[]) {
 		printf ("Proceso padre: PID %d \n", getpid());
 	}
 	return 0;
+}*/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <pthread.h>
+
+void* hola(void* arg){
+	char* msg = "Hola";
+	int i;
+	for (i=0; i<strlen(msg); i++){
+		printf("%c", msg[i]) ;
+		fflush(stdout) ;
+		usleep(1000000) ;
+	}
+	return NULL;
+}
+
+void* mundo(void* arg){
+	char* msg = " mundo";
+	int i;
+	for (i=0; i<strlen(msg); i ++){
+		printf("%c", msg[i]);
+		fflush(stdout);
+		usleep(1000000);
+	}
+	return NULL;
+}
+
+int main(int argc, char* argv[]){
+	pthread_t h1;
+	pthread_t h2;
+	pthread_create(&h1, NULL, hola, NULL);
+	pthread_create(&h2, NULL, mundo, NULL);
+	pthread_join(h1, NULL);
+	pthread_join(h2, NULL);
+	printf("Fin\n");
 }
