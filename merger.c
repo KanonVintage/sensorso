bf215be0df3873e2f45fdd9fc946f8c1a8550dcb
KanonVintage <guido.duchi@gmail.com>
2019-02-01 02:50:58 -0500

    phase 1 completed

--- merger.c
+++ merger.c
@@ -18,7 +18,7 @@ int main()
     char* ptr;
     int cont=0;
 
-    strcpy(filename,"../startup.x");
+    strcpy(filename,"compiled/startup.x");
 
     fp = fopen(filename, "r");
     if (fp == NULL){
@@ -27,6 +27,29 @@ int main()
     }
 
     pid_t pids[3];
+
+    while (fgets(str, MAXCHAR, fp) != NULL){
+        pid_t pid=fork();
+        pids[cont]=pid;
+        //printf("%s", str);
+        sscanf(str, "%[^:]:%d,%d", name, &keyd, &keyt);
+        sprintf(keycd,"%d",keyd);
+        sprintf(keyct,"%d",keyt);
+        //printf("name:%s  keyd:%s  keyt:%s\n", name,keycd,keyct);
+        if (pid==0) { /* child process */
+            //char *argv[]={"1", name,"3"};
+            execl("compiled/sensorSO","argv",name,keycd,keyct,(char *)NULL);
+            //execl("/usr/bin/terminator", "terminator", "-e", controllers, NULL);
+
+            exit(127); /* only if execv fails */
+        }
+        printf("[%d] [%d] i=%d\n", getppid(), getpid(), cont);
+        cont++;
+    }
+
+    sleep(1);
+    rewind(fp);
+    cont=0;
 
     while (fgets(str, MAXCHAR, fp) != NULL){
         pid_t pid=fork();
@@ -48,28 +71,6 @@ int main()
             //char *argv[]={"1", name,"3"};
             //execl("compiled/sensorSO","argv",name,keycd,keyct,(char *)NULL);
             execl("/usr/bin/terminator", "terminal", "-e", controllers, NULL);
-
-            exit(127); /* only if execv fails */
-        }
-        printf("[%d] [%d] i=%d\n", getppid(), getpid(), cont);
-        cont++;
-    }
-
-    rewind(fp);
-    cont=0;
-
-    while (fgets(str, MAXCHAR, fp) != NULL){
-        pid_t pid=fork();
-        pids[cont]=pid;
-        //printf("%s", str);
-        sscanf(str, "%[^:]:%d,%d", name, &keyd, &keyt);
-        sprintf(keycd,"%d",keyd);
-        sprintf(keyct,"%d",keyt);
-        //printf("name:%s  keyd:%s  keyt:%s\n", name,keycd,keyct);
-        if (pid==0) { /* child process */
-            //char *argv[]={"1", name,"3"};
-            execl("compiled/sensorSO","argv",name,keycd,keyct,(char *)NULL);
-            //execl("/usr/bin/terminator", "terminator", "-e", controllers, NULL);
 
             exit(127); /* only if execv fails */
         }
